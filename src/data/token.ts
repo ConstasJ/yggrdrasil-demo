3bbb02dd096b23adedba6dec87cded4ebd4752c3
ConstasJ <constasj@qq.com>
2024-03-02 15:50:24 +0800

    feat: 完成Token模型编写

---
+++ src/data/token.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from "sequelize";
+import { db } from "./init";
+
+class Token extends Model {
+    declare public accessToken:string;
+    declare public clientToken: string;
+    declare public playerUUID: string;
+    declare public issuedTime: Date;
+}
+
+Token.init({
+    accessToken: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    clientToken: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    playerUUID: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    issuedTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+}, {
+    sequelize: db
+});
+
+Token.sync({alter: true});
