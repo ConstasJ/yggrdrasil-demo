67b0929dc88240ed61fd37edd7da407fded181ad
ConstasJ <constasj@qq.com>
2024-03-02 15:36:10 +0800

    feat: 完成玩家模型

---
+++ src/data/profile.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from "sequelize";
+import { db } from "./init";
+
+type TextureType = 'SKIN'|'CAPE';
+type Texture = {
+    [key in keyof TextureType]: string
+}
+
+class Profile extends Model {
+    declare public id: string;
+    declare public name: string;
+    declare public model: 'default' | 'slim';
+    declare public texture: Texture;
+}
+
+Profile.init({
+    id:{
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    model: {
+        type: DataTypes.ENUM('default', 'slim'),
+        allowNull: false,
+    },
+    texture: {
+        type: DataTypes.JSON,
+        allowNull: false,
+    }
+}, {
+    sequelize: db
+});
+
+Profile.sync({alter:true});
