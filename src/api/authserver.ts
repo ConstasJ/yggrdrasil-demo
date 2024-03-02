5bef4a9ba1bfa8b742c4cc67ba963349c540b80d
ConstasJ <constasj@qq.com>
2024-03-02 21:08:50 +0800

    feat: 完成/authenticate部分逻辑

--- src/api/authserver.ts
+++ src/api/authserver.ts
@@ -1,8 +1,33 @@
 import Router from "koa-router";
 
+import { AuthenticateRequest } from "../types";
+import { Profile } from "../data";
+
 const authServerApi = new Router();
 
 authServerApi.post('/authenticate', async (ctx) => {
+    const body = ctx.request.body as AuthenticateRequest;
+    try{
+        const profile = await Profile.findOne({
+            where: {
+                name: body.username
+            }
+        });
+        if(!profile){
+            ctx.res.statusCode = 404;
+            ctx.body = {
+                error: "ENOTFOUND",
+                errorMessage: "User not found"
+            }
+        }
+        
+    } catch(e) {
+        ctx.status = 500;
+        ctx.body = {
+            error: "ESRVERR",
+            errorMessage: "Internal server error"
+        }
+    }
     
 })
 
