285c26e2c56565bb735962950af307c8e20a782a
ConstasJ <constasj@qq.com>
2024-03-03 15:27:15 +0800

    feat: 添加UUID与无符号UUID相互转换的实用函数

--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,12 @@ function generateAcessToken(){
     return Math.random().toString(36).slice(-30);
 }
 
+function signedToUnsignedUUID(uuid:string) {
+    return uuid.replace(/-/g, '');
+}
+
+function unsignedToSignedUUID(uuid:string) {
+    return uuid.replace(/^(.{8})(.{4})(.{4})(.{4})(.{12})$/, '$1-$2-$3-$4-$5');
+}
+
 export { generateAcessToken };
